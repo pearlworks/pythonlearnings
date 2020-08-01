faa496252f7964994c7726b9765827a4a061aecf
Shanmuga Narayanan Pitchaipillai <narainelearns@gmail.com>
2020-08-01 21:45:22 +0000

    added few basic learning modules

---
+++ pythonbasics/ryanpybasics.py
@@ -0,0 +1,7 @@
+#https://github.com/aneagoie/ztm-python-cheat-sheet
+print("Shan narayan learns python")
+#saidstatement=input("what do you want to say?")
+#print("what was said is:" + saidstatement)
+teststring = 'abcefghijklm'
+print (teststring[0:5])
+print (teststring[0:10:2])
